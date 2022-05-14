cd97ffdd41a1ef717eb9d21b2d112efe6920a341
Mauricio F <mauriciofsnts@gmail.com>
2022-05-14 12:54:51 -0300

    refactor: remove useless stuff

--- internal/discord/main.go
+++ internal/discord/main.go
@@ -10,31 +10,27 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-func Start() {
+func Start() error {
 
-	// Create a new Discord session using the provided bot token.
 	dg, err := discordgo.New("Bot " + config.Wombot.Token)
+
 	if err != nil {
-		fmt.Println("error creating Discord session,", err)
-		return
+		return err
 	}
 
-	// In this example, we only care about receiving message events.
 	dg.Identify.Intents = discordgo.PermissionAdministrator
 
-	// Open a websocket connection to Discord and begin listening.
 	err = dg.Open()
 	if err != nil {
-		fmt.Println("error opening connection,", err)
-		return
+		return err
 	}
 
-	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
 
-	// Cleanly close down the Discord session.
 	dg.Close()
+
+	return nil
 }
